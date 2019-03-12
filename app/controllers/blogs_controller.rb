80c5c382ac5c70ba338cc41373f680a63c7a8204
Hiro Sakurada <sanno.phill@gmail.com>
2019-03-12 03:51:06 +0000

    編集機能・削除機能・確認画面機能を追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,15 +1,48 @@
 class BlogsController < ApplicationController
+  before_action :set_blog, only: [:edit, :update, :destroy]
+  
   def index
     @blogs = Blog.all
   end
   
   def new
-    @blog = Blog.new
+    if params[:back]
+      @blog = Blog.new(blog_params)
+    else
+      @blog = Blog.new
+    end
   end
 
   def create
-    Blog.create(blog_params)
-    redirect_to new_blog_path
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to new_blog_path, notice: "つぶやき成功！"
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+  
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path, notice: "つぶやきを編集しました！"
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @blog.destroy
+    redirect_to blogs_path, notice: "つぶやきを削除しました！"
+  end
+  
+  def confirm
+    @blog = Blog.new(blog_params)
+    render :new if @blog.invalid?
   end
   
   private
@@ -17,4 +50,8 @@ class BlogsController < ApplicationController
   def blog_params
     params.require(:blog).permit(:content)
   end
+  
+  def set_blog
+    @blog = Blog.find(params[:id])
+  end  
 end
